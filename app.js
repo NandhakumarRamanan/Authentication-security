4db215139b010e55899fbf8396663a309fcc808c
unknown <nandha1181@gmail.com>
2022-10-19 05:03:09 +0530

    Add env variables

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt= require("mongoose-encryption");
 
-const app= express();
+const app=express();
 
 mongoose.connect("mongodb://localhost:27017/useDB");
 
@@ -14,8 +15,7 @@ const userSchema =new mongoose.Schema({
   password:String
 });
 
-const secret = "Thisisourlittlesecret."
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
